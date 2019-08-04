4a625c0fd13a20cd7f3a6d29a19dd75bc04874c9
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-08-03 20:17:21 -0600

    added click sound effect to buttons

--- calc.js
+++ calc.js
@@ -4,3 +4,9 @@ import { plus } from './operations.js';
 //Try different variations between 1 and 10
 
 console.log("one(plus(one())) returns", one(plus(one())))
+
+//add sound to button
+let click = new Audio("/Button-click-sound-effect.mp3");
+let buttons = document.querySelectorAll("button")
+
+buttons.forEach(button => button.addEventListener("click", () => click.play()));
