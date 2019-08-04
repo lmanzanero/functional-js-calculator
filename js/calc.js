06b6254211e7baea94cc9809ca5ea0d052a9455c
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-08-03 22:01:22 -0600

    Added a simple cheat version with eval() to do the math

--- js/calc.js
+++ js/calc.js
@@ -8,5 +8,38 @@ console.log("one(plus(one())) returns", one(plus(one())))
 //add sound to button
 let click = new Audio("Button-click-sound-effect.mp3");
 let buttons = document.querySelectorAll("button")
+let equalButton = document.querySelector("button.equals");
+//input screen
+let inputString = document.querySelector("input");
+let calcString = ""; 
 
-buttons.forEach(button => button.addEventListener("click", () => click.play()));
+buttons.forEach(button =>{ 
+    button.addEventListener("click", (e) =>{ 
+        click.play()
+     let buttonNumber =  e.target.innerHTML;
+     //prevent equal to be added to string 
+     if(buttonNumber === equalButton.innerHTML){
+         return "";
+     } else {
+        //show new calcString to input screen 
+        addNumbersToScreen(buttonNumber) 
+     }
+    });
+});
+
+//Add Final value to screen and cheat the math with Eval
+equalButton.addEventListener("click", () => { 
+    //cheat the math with eval()
+    //update screen with final value
+    inputString.value = eval(calcString); 
+});
+ 
+//updateInputScreen 
+const addNumbersToScreen = (number) => { 
+    calcString += number;
+    inputString.value = calcString;
+}
+//detect new text on input
+inputString.addEventListener("input", (e) => console.log(e.target.value));
+
+console.log(inputString);
